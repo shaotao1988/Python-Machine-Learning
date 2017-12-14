cb67387652aa1fc33124bdf9ecbc3014da0b6d20
shaotao1988 <563725203@qq.com>
2017-12-14 00:12:46 -0500

    增加正规化

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+
 class Perceptron(object):
     """Perceptron classifier.
 
@@ -21,7 +22,7 @@ class Perceptron(object):
         Number of misclassifications in each epoch
     """
 
-    def __init__(self, eta = 0.01, n_iter = 10):
+    def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
         self.w_ = []
@@ -30,80 +31,57 @@ class Perceptron(object):
     def fit(self, X, y):
         """Fit training data
         :param X: Training data, shape = [n_samples, n_features]
-        :param y: Target values, shape = [n_examples]
+        :param y: Target values, shape = [n_samples]
         :return: self
         """
-        self.w_ = np.zeros(X.shape[1] + 1)
-        self.errors = np.zeros(self.n_iter)
+        self.w_ = np.zeros((X.shape[1] + 1, 1))
+        self.errors = np.zeros((self.n_iter, 1))
+        X = np.insert(X, 0, 1, 1) #Insert x0
         for i in range(self.n_iter):
-            update = self.eta*(self.predict(X) - y)
-            self.w_[1:] -= np.dot(X.T, update)
-            self.w_[0] -= sum(update)
-            self.errors[i] = np.dot(update, update)/X.shape[0]
+            output = self.net_input(X)
+            error = output - y
+            update = self.eta * error
+            self.w_ -= np.dot(X.T, update)
+            self.errors[i] = np.dot(error.T, error) / 2
         return self
 
     def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+        return np.dot(X, self.w_)
 
     def predict(self, X):
-        positive = np.ones(X.shape[0])
-        negative = -1*positive
-        return np.where(self.net_input(X) > 0, positive, negative)
+        X = np.insert(X, 0, 1, 1)  # Insert x0
+        return np.where(self.net_input(X) > 0, 1, -1)
 
-def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    def normalize(self, X):
+        X = (X-X.mean(0))/X.std(0)
+        return X
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
     markers = ('s', 'o', 'x', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-    #plot the decision surface
+    # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1],
-                    alpha = 0.8, c=cmap(idx),
-                    marker = markers[idx], label = cl)
-
-
 if __name__ == "__main__":
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
     y = df.iloc[0:100, 4].values
-    y = np.where(y == 'Iris-setosa', -1, 1)
+    y = np.where(y == 'Iris-setosa', -1, 1).reshape(y.shape[0], 1)
     X = df.iloc[0:100, [0, 2]].values
-    plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
-    plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
-    plt.legend(loc = 'upper left')
-    Classifier = Perceptron()
-    Classifier.fit(X, y)
-    #plt.plot(Classifier.errors)
-    plot_decision_regions(X, y, classifier = Classifier)
+    Classifier = Perceptron(0.001, 100)
+    X_std = Classifier.normalize(X)
+    plt.scatter(X_std[:50, 0], X_std[:50, 1], color='red', marker='o', label='setosa')
+    plt.scatter(X_std[50:100, 0], X_std[50:100, 1], color='blue', marker='x', label='versicolor')
+    plt.legend(loc='upper left')
+    Classifier.fit(X_std, y)
+    Z = Classifier.predict(X_std)
+    plot_decision_regions(X_std, y, classifier=Classifier)
     plt.show()
-    print('finished')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
